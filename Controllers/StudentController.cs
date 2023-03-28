814548f93ae3793b934f510e5eb6157014bb46d4
Basant_Elolemy <belolemy@gmail.com>
2023-03-28 22:52:38 +0200

    student

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,34 @@
+﻿using Examination_system.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Examination_system.Controllers
+{
+    public class StudentController : Controller
+    {
+
+        IqexamContext DB = new IqexamContext();
+
+        //display courses by student id
+        
+
+        public IActionResult GetCourseByStuId(int id)
+        {
+
+            List<Course> crs = DB.Courses.FromSql($"Sp_GetStuCoursesByID {id}").ToList();
+            return View (crs);
+        }
+
+        public IActionResult GetExamByCrsId(int id)
+        {
+            var questions = DB.Questions.FromSql($"sp_get_exam_by_courseId {id}").ToList();
+
+            return Json(questions);
+        }
+           public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
